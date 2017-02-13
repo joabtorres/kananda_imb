eb1461680ca300f97bdb11465771bba1917f70a7
Joab Torres <joabtorres1508@gmail.com>
2017-02-13 01:23:22 -0300

    ajuste no .htaccess

--- core/core.php
+++ core/core.php
@@ -3,14 +3,11 @@
 class core {
 
     public function run() {
-        $url = explode('index.php', $_SERVER['PHP_SELF']);
-        $url = end($url);
-
+        $url = $_GET['url'];
         $params = array();
 
         if (!empty($url) && $url != '/') {
             $url = explode('/', $url);
-            array_shift($url);
             global $painel;
             if (isset($url[0]) && $url[0] == "painel_admin") {
                 $painel = TRUE;
